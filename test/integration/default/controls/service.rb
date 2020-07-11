46bfe11337c1239d16e20d8fcf1ce1517bd5b235
Javier Bértoli <javier@netmanagers.com.ar>
2020-07-11 09:32:31 -0300

    refactor(semantic-release): align to template-formula

--- test/integration/default/controls/service.rb
+++ test/integration/default/controls/service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 control 'rspamd service' do
   impact 1.0
   title 'Manage the Rspamd Service'
@@ -25,24 +27,26 @@ control 'rspamd service' do
 
   # We seem to be hiting this
   if os[:family] == 'debian'
-    %w{
+    %w[
       11332
       11333
-    }.each do |pn|
-      # It takes a while to the port to be listening,
-      # se we add a little sleep
-      sleep(5)
+      11334
+    ].each do |pn|
       describe port(pn) do
+        # It takes a while to the port to be listening,
+        # se we add a little sleep
+        before do
+          30.times do
+            unless port(pn).listening?
+              puts "Port #{pn} isn't ready, retrying.."
+              sleep 1
+            end
+          end
+        end
         it { should be_listening }
         its('protocols') { should include 'tcp' }
-        its('addresses') { should include '0.0.0.0' }
+        its('addresses') { should include '127.0.0.1' }
       end
-    end
-  
-    describe port(11334) do
-      it { should be_listening }
-      its('protocols') { should include 'tcp' }
-      its('addresses') { should include '127.0.0.1' }
     end
   end
 end
